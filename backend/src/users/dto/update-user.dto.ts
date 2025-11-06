e944d144d0d0f943408f9f0b8294ea1c4616113b
hectororviz <hector.h.orviz@gmail.com>
2025-11-06 12:13:13 -0300

    Revert "Add optional club assignment for users"

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
@@ -8,8 +8,4 @@ export class UpdateUserDto {
   @IsOptional()
   @IsString()
   lastName?: string;
-
-  @IsOptional()
-  @IsInt()
-  clubId?: number | null;
 }
